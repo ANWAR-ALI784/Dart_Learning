b5008445d10432131509f9a92ec120993b00aff8
ANWAR-ALI784 <anwaralimarwat39@gmail.com>
2026-02-07 22:35:59 +0500

    Initial commit: Dart language learning with Flutter

---
+++ android/app/src/main/kotlin/com/example/dartlanguage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dartlanguage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/dartlanguage/variabletype.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  const Text("Login");   // UI fixed bcz const
+
+  final userName = "Ali";   // from user final
+
+  var count = 0;    // changes var
+
+  dynamic apiData;   // from server dynamic
+
+  // var use
+  var age=25;
+  print(age);
+  var city="lahore";
+  print(city);
+  dynamic v=34;
+  v="anwar";
+  print(v);
+  final a=45.6;
+  print(a);
+  const pi='welcome';
+  print(pi);
+
+
+}
